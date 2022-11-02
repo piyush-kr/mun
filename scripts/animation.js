4159c42e68a2ad8dabb94a97dca18bcc56637c30
Prince <63097096+piyush-kr@users.noreply.github.com>
2022-11-02 22:00:55 +0530

    animation remove

--- scripts/animation.js
+++ scripts/animation.js
@@ -15,11 +15,11 @@ tl.from(".heroContent h1", { x: 500, duration: 2, opacity: 0, ease: "power4.out"
 tl.from(".heroContent h2", { duration: 1, opacity: 0, y: 100, ease: "power4.out" })
 tl.from(".heroContent h3", { duration: .5, opacity: 0, x: 100, ease: "power4.out" })
 
-gsap.from(".themeheading", { scrollTrigger: ".themeheading", x: 700, duration: 2, ease: "power4.out" })
-gsap.from(".theme p", { scrollTrigger: ".themeheading", y: 300, duration: 2, opacity: 0, ease: "power4.out", delay: 1 })
-gsap.from(".tagline", { scrollTrigger: ".tagline", scale: 2, x: -400, duration: 2, opacity: 0, ease: "power4.out", delay: 0 })
-gsap.from(".aboutMun p", { scrollTrigger: ".aboutMun p", scale: 2, duration: 2, opacity: 0, ease: "power4.out" })
-gsap.from(".aboutMun img", { scrollTrigger: ".aboutMun img", y: 500, duration: 1, opacity: 0, ease: "power4.out" })
+// gsap.from(".themeheading", { scrollTrigger: ".themeheading", x: 700, duration: 2, ease: "power4.out" })
+// gsap.from(".theme p", { scrollTrigger: ".themeheading", y: 300, duration: 2, opacity: 0, ease: "power4.out", delay: 1 })
+// gsap.from(".tagline", { scrollTrigger: ".tagline", scale: 2, x: -400, duration: 2, opacity: 0, ease: "power4.out", delay: 0 })
+// gsap.from(".aboutMun p", { scrollTrigger: ".aboutMun p", scale: 2, duration: 2, opacity: 0, ease: "power4.out" })
+// gsap.from(".aboutMun img", { scrollTrigger: ".aboutMun img", y: 500, duration: 1, opacity: 0, ease: "power4.out" })
 gsap.from(".committeeGroup img", { scrollTrigger: ".committeeGroup", y: 500, scale: 5, duration: 2, delay: 1, opacity: 0, ease: "power4.out" })
 
 
