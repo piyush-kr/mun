4e974d9cd7a1004ad1c589e16576612223a610db
Prince <63097096+piyush-kr@users.noreply.github.com>
2022-11-02 20:56:44 +0530

    init

---
+++ scripts/animation.js
@@ -0,0 +1,27 @@
+let tl = gsap.timeline()
+
+// add the tweens to the timeline - Note we're using tl.to not gsap.to
+if (window.innerWidth > 1062) {
+    tl.from(".navigation-logo", { duration: 1, scale: 4 })
+    tl.from(".nav1", { x: -100, duration: .3, opacity: 0, ease: "back.out(1.7)" });
+    tl.from(".nav2", { x: -100, duration: .3, opacity: 0, ease: "back.out(1.7)" });
+    tl.from(".nav3", { x: -100, duration: .2, opacity: 0, ease: "back.out(1.7)" });
+    tl.from(".nav4", { x: -100, duration: .2, opacity: 0, ease: "back.out(1.7)" });
+    tl.from(".nav5", { x: -100, duration: .2, opacity: 0, ease: "back.out(1.7)" });
+    tl.from(".nav6", { x: -100, duration: .2, opacity: 0, ease: "back.out(1.7)" });
+    tl.from(".nav7", { x: -100, duration: .2, opacity: 0, ease: "back.out(1.7)" });
+}
+tl.from(".heroContent h1", { x: 500, duration: 2, opacity: 0, ease: "power4.out" })
+tl.from(".heroContent h2", { duration: 1, opacity: 0, y: 100, ease: "power4.out" })
+tl.from(".heroContent h3", { duration: .5, opacity: 0, x: 100, ease: "power4.out" })
+
+gsap.from(".themeheading", { scrollTrigger: ".themeheading", x: 700, duration: 2, ease: "power4.out" })
+gsap.from(".theme p", { scrollTrigger: ".themeheading", y: 300, duration: 2, opacity: 0, ease: "power4.out", delay: 1 })
+gsap.from(".tagline", { scrollTrigger: ".tagline", scale: 2, x: -400, duration: 2, opacity: 0, ease: "power4.out", delay: 0 })
+gsap.from(".aboutMun p", { scrollTrigger: ".aboutMun p", scale: 2, duration: 2, opacity: 0, ease: "power4.out" })
+gsap.from(".aboutMun img", { scrollTrigger: ".aboutMun img", y: 500, duration: 1, opacity: 0, ease: "power4.out" })
+gsap.from(".committeeGroup img", { scrollTrigger: ".committeeGroup", y: 500, scale: 5, duration: 2, delay: 1, opacity: 0, ease: "power4.out" })
+
+
+
+
---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+
+
+
+const hamburger1 = document.querySelector(".hamburger");
+function hamburger() {
+    hamburger1.classList.toggle("is-active");
+    document.querySelector(".navigationMain").classList.toggle("navActive");
+    document.querySelector(".hamburgerMenu").classList.toggle("hamburgerActive");
+    document.querySelector(".m-img").classList.toggle("m-imgBlur");
+    document.body.classList.toggle("overflowNone");
+}
+
+
+
+
+
+
+
+
+
+
+
+
